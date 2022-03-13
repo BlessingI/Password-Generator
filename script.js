7882c2be95405ac1d25e75ee9ef94d0e827ecaaf
Blessing <bigbadumhe@gmail.com>
2022-03-12 22:34:42 -0600

    included Readme

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+//   providing series of prompts for password generation
+  var promptFight = window.prompt('Would you like your password to be scalet or unscalet? Enter "SCALET" OR "UNSCALET" to chose');
+  promptFight = promptFight.toUpperCase()
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
