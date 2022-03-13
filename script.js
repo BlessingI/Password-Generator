b69528ce5421d4ee596dd6d4ecd1113c2c07ee20
Blessing <bigbadumhe@gmail.com>
2022-03-13 01:36:13 -0600

    all true

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
 //   providing series of prompts for password generation
-  var promptFight = window.prompt('Would you like your password to be scalet or unscalet? Enter "SCALET" OR "UNSCALET" to chose');
-  promptFight = promptFight.toUpperCase()
+  var promptPassword = window.prompt('How many characters would you like your password? Choose between 8 and 128');
+  if(promptPassword >= 8 && promptPassword <= 128) {
+      var confirmLowerCase = confirm('Will this contain lowercase letters?')
+        if(confirmLowerCase){
+            //if confirmlowercase is true, check if confirmUpperCase is true
+            var confirmUpperCase = confirm('Will this contain uppercase letters?')
+            if(confirmUpperCase){
+                //if it contains lowercase and uppercase
+                var confirmNumber = confirm('Will this contain numbers?')
+                if(confirmNumber){
+                    //if it contains lowercase, uppercase and number
+                    var confirmSpecialCharacter =confirm('Will this contain special characters?')
+                    if(confirmSpecialCharacter){
+                        //if it contains lowercase, uppercase, number and special character
+                        alert("The password")
+                    } else {
+                        //if it contains lowercase, uppercase, number but no special character
+                    }
+                } else {
+                    //if it contains lowercase, uppercase but no number
+                }
+            } else {
+                //if it contains lowercase but no uppercase
+            }
+        } else {
+            //if confirm lowercase is false, check if confirmUpperCaseb is true
+        }
+
+
+      
+
+  } else if(promptPassword < 8 || promptPassword > 128) {
+      alert('You must choose between 8 and 128')
+  } 
 
 
 
