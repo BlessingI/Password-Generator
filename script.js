38f6a133e90fd50fce7e85de2a5872298721d476
Blessing <bigbadumhe@gmail.com>
2022-03-13 20:28:40 -0500

    completed project

--- script.js
+++ script.js
@@ -296,12 +296,6 @@ function writePassword() {
   }
 
 
-
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
